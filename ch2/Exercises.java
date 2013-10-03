80150bcd66829e453d00f245d596a1fe9fc0e5c4
Jason Jozwiak <jjozwiak@gacommunication.com>
2013-10-02 21:41:32 -0500

    committing chapter 2 exercises

---
+++ ch2/Exercises.java
@@ -0,0 +1,15 @@
+public class Exercises {
+    public static void main(String[] args) {
+
+        /**
+         *	Exercise1 Name, Age, and Annual Income
+         */
+        
+        String name = "Jason Jozwiak";
+        int age = 28;
+        double annualPay = 1000000.00;
+
+        System.out.println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " a year.");
+
+    }
+}

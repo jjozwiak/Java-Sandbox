0de4ee4a0708f3aabcf9d5323855d3e0dc536b72
Jason Jozwiak <jjozwiak@gacommunication.com>
2013-10-22 23:00:59 -0500

    committing program 3

--- assignment1/Assn1Prog3.java
+++ assignment1/Assn1Prog3.java
@@ -1,10 +1,62 @@
 import java.util.Scanner;
 
 public class Assn1Prog3 {
-    public static void main(String[] args){
+	public static void main(String[] args){
 
+		double futureValue = 0;
+		double interestRate = 0;
+		double numberOfYears = 0;
+		double currentValue = 0;
+		double presentValue = 0;
+		int iterator = 0;
 
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter a future value");
 
+		currentValue = keyboard.nextDouble();
 
+		while(currentValue >= 0)
+		{
+			switch(iterator)
+			{	
+				case 0:
+    				futureValue = currentValue;
+    				System.out.println("Please enter an interest rate");
+    				currentValue = keyboard.nextDouble();
+    				break;
+    			case 1:
+    				interestRate = currentValue;
+    				System.out.println("Please enter number of years");
+    				currentValue = keyboard.nextDouble();
+    				break;
+    			case 2:
+    				numberOfYears = currentValue;
+    				currentValue = -1;
+    				break;
+    		}
+
+    		++iterator;
+    	}
+
+    	if(iterator < 3)
+    	{
+    		System.out.println("This program is ending because " + currentValue + " is an invalid input.");
+    	}
+    	else
+    	{
+    		presentValue = presentValue(futureValue, interestRate, numberOfYears);
+    		System.out.printf("Your present value is: %f \n", presentValue);
+    	}
+
+    }
+
+    public static double presentValue(double futureValue, double interestRate, double numberOfYears)
+    {
+    	double presentValue;
+
+    	//Math.pow(4.0, 2.0)
+    	presentValue = futureValue/Math.pow((1 + interestRate), numberOfYears);
+
+    	return presentValue;
     }
 }

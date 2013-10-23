bcf03488dba93ed751b0944bcab4458249b9a556
Jason Jozwiak <jjozwiak@gacommunication.com>
2013-10-22 21:44:08 -0500

    committing assignment 1 program 2

--- assignment1/Assn1Prog2.java
+++ assignment1/Assn1Prog2.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Assn1Prog2 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
 
         double distance;
+        int menuItemSelected;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Please enter a positive number for distance");
+        System.out.println("Enter a distance in meters: ");
 
         distance = keyboard.nextDouble();
 
@@ -18,6 +20,75 @@ public class Assn1Prog2 {
             distance = keyboard.nextDouble();
         }
 
-        System.out.println("The distance is " + distance + " meters.");
+        menu();
+
+        menuItemSelected = keyboard.nextInt();
+
+        while(menuItemSelected < 1 || menuItemSelected > 4){
+            System.out.println("Sorry " + menuItemSelected + " is not a valid menu option. Please select from one of the following:");
+            menu();
+            menuItemSelected = keyboard.nextInt();
+        }
+
+        while(menuItemSelected != 4){
+
+            switch(menuItemSelected)
+            {
+                case 1:
+                    showKilometers(distance);
+                    break;
+                case 2:
+                    showInches(distance);
+                    break;
+                case 3:
+                    showFeet(distance);
+                    break;
+            }
+
+            menu();
+
+            menuItemSelected = keyboard.nextInt();
+
+            while(menuItemSelected < 1 || menuItemSelected > 4){
+                System.out.println("Sorry " + menuItemSelected + " is not a valid menu option. Please select from one of the following:");
+                menu();
+                menuItemSelected = keyboard.nextInt();
+            }
+
+        }
+
+
+    }
+
+    public static void showKilometers(double meters)
+    {
+        double kilometers;
+        kilometers = meters * 0.001;
+        System.out.printf("\n%f meters is %f kilometers\n", meters, kilometers);
+    }
+
+    public static void showInches(double meters)
+    {
+        double inches;
+        inches = meters * 39.37;
+        System.out.printf("\n%f meters is %f inches\n", meters, inches);
+    }
+
+    public static void showFeet(double meters)
+    {
+        double feet;
+        feet = meters * 3.281;
+        System.out.printf("\n%f meters is %f feet\n", meters, feet);
+    }
+
+    public static void menu()
+    {
+        System.out.println();
+        System.out.println("1. Convert to kilometers");
+        System.out.println("2. Convert to inches");
+        System.out.println("3. Convert to feet");
+        System.out.println("4. Quit the program");
+        System.out.println();
+        System.out.println("Enter your choice:");
     }
 }

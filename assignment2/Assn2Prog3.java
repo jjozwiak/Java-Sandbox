7c09931c1d14a7d2ee88c93f12d8133e2bb4040d
Jason Jozwiak <jjozwiak@gacommunication.com>
2013-10-30 21:10:24 -0500

    updating program 3 of assignment 2

--- assignment2/Assn2Prog3.java
+++ assignment2/Assn2Prog3.java
@@ -3,34 +3,91 @@ import java.io.*;
 
 public class Assn2Prog3 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {      
 
-        String name = "John Smith";
-        double grade1 = 70;
-        double grade2 = 80;
-        double grade3 = 90;
-        double grade4 = 100;
+        String name = "";
+        double grade1 = 0;
+        double grade2 = 0;
+        double grade3 = 0;
+        double grade4 = 0;
+
+        int numberOfLines = 0;
+        int numberOfStudents = 0;
+        int iterator = 1;
+
+        double grade1ClassTotals = 0;
+        double grade2ClassTotals = 0;
+        double grade3ClassTotals = 0;
+        double grade4ClassTotals = 0;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Please enter the name of a file without the file extension (ex: .txt): ");
-
+        System.out.print("Please enter the name of a file including the file extension (ex: .txt): ");
         String fileName = keyboard.nextLine();
 
-        while(! fileName.equals("filename"))
+        File gradeFile = new File(fileName);
+
+        //Exit the program if the file does not exist
+        if(!gradeFile.exists())
         {
-            System.out.println("Sorry, a file of that name does not exist.");
-            System.out.print("Please enter the name of another file: ");
-            fileName = keyboard.nextLine();
+            System.out.println("The file name you entered does not exist.");
+            System.exit(0);
         }
 
-        double average = calculateAverage(grade1, grade2, grade3, grade4);
+        Scanner inputFile2 = new Scanner(gradeFile);
 
-        char grade = calculateGrade(average);
+        while(inputFile2.hasNext())
+        {
 
-        System.out.println(name + "'s four grades are: " + grade1 + ", " + grade2 + ", " + grade3 + ", and " + grade4 + ". The grade average is: " + average + " and the letter grade is: " + grade + ".");
+            ++numberOfLines;
+            if(iterator == 5)
+            {
+                grade4 = Double.parseDouble(inputFile2.nextLine());
 
+                double average = calculateAverage(grade1, grade2, grade3, grade4);
+                char grade = calculateGrade(average);
+
+                System.out.println(name + "'s four grades are: " + grade1 + ", " + grade2 + ", " + grade3 + ", and " + grade4 + ". The grade average is: " + average + " and the letter grade is: " + grade + ".");
+
+                grade4ClassTotals += grade4;
+                iterator = 1;
+            }
+            else
+            {
+
+                if(iterator == 1){
+                    name = inputFile2.nextLine();
+                }
+                else if (iterator == 2)
+                {
+                    grade1 = Double.parseDouble(inputFile2.nextLine());
+                    grade1ClassTotals += grade1;
+                }
+                else if (iterator == 3)
+                {
+                    grade2 = Double.parseDouble(inputFile2.nextLine());
+                    grade2ClassTotals += grade2;
+                }
+                else
+                {
+                    grade3 = Double.parseDouble(inputFile2.nextLine());
+                    grade3ClassTotals += grade3;
+                }
+                ++iterator;
+            }
+        }
+
+        System.out.println();
+
+        numberOfStudents = numberOfLines/5;
+
+        System.out.println("Grade 1 Class Average: " + grade1ClassTotals/numberOfStudents);
+        System.out.println("Grade 2 Class Average: " + grade2ClassTotals/numberOfStudents);
+        System.out.println("Grade 3 Class Average: " + grade3ClassTotals/numberOfStudents);
+        System.out.println("Grade 4 Class Average: " + grade4ClassTotals/numberOfStudents);
+
+        inputFile2.close();
     }
     /**
      * [calculateAverage description]
@@ -50,7 +107,11 @@ public class Assn2Prog3
 
         return average;
     }
-
+    /**
+     * [calculateGrade description]
+     * @param  average [description]
+     * @return         [description]
+     */
     public static char calculateGrade(double average)
     {
         if(average >= 90)
